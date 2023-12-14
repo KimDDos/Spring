ca5001cd7d38e8f36b0281e9649d4c450fa76f5d
Doyeon_KIm <126250672+KimDDos@users.noreply.github.com>
2023-12-14 15:15:07 +0900

    second commit

---
+++ _Spring_Workspace/Spring_Test/src/main/java/com/ezen/www/domain/BoardVO.java
@@ -0,0 +1,22 @@
+package com.ezen.www.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class BoardVO {
+	private int bno;
+	private String title;
+	private String writer;
+	private String content;
+	private String isDel;
+	private String regdate;
+	private int read_count;
+}
